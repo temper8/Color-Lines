f8a7246edc88ebdb3f05c75c78433fc43c9b4fb8
Alex <temper8@gmail.com>
2017-04-11 13:02:25 +0300

    add TView

---
+++ src/TView.cpp
@@ -0,0 +1,18 @@
+/*
+ * TView.cpp
+ *
+ *  Created on: Apr 11, 2017
+ *      Author: Alex
+ */
+
+#include "TView.h"
+
+TView::TView() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TView::~TView() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/TView.h
@@ -0,0 +1,17 @@
+/*
+ * TView.h
+ *
+ *  Created on: Apr 11, 2017
+ *      Author: Alex
+ */
+
+#ifndef TVIEW_H_
+#define TVIEW_H_
+
+class TView {
+public:
+	TView();
+	virtual ~TView();
+};
+
+#endif /* TVIEW_H_ */

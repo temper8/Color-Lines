f2513bbd0fa52be1c14b615c9c48016e200a12e5
Alex <temper8@gmail.com>
2017-04-12 18:28:50 +0300

    add TLinesBoard

---
+++ src/TLinesBoard.cpp
@@ -0,0 +1,19 @@
+/*
+ * TLinesBoard.cpp
+ *
+ *  Created on: Apr 12, 2017
+ *      Author: Alex
+ */
+
+#include "TLinesBoard.h"
+
+TLinesBoard::TLinesBoard(int x, int y) {
+	// TODO Auto-generated constructor stub
+	sizeX = x;
+	sizeY = y;
+}
+
+TLinesBoard::~TLinesBoard() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/TLinesBoard.h
@@ -0,0 +1,18 @@
+/*
+ * TLinesBoard.h
+ *
+ *  Created on: Apr 12, 2017
+ *      Author: Alex
+ */
+
+#ifndef TLINESBOARD_H_
+#define TLINESBOARD_H_
+
+class TLinesBoard {
+public:
+	TLinesBoard(int x,int y);
+	virtual ~TLinesBoard();
+	int sizeX, sizeY;
+};
+
+#endif /* TLINESBOARD_H_ */

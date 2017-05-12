343e4f578fa10db72acd237e869a5497fb648f91
Alex <temper8@gmail.com>
2017-05-12 17:06:55 +0300

    добавил отладочный лог

--- src/TPreferenceInt.cpp
+++ src/TPreferenceInt.cpp
@@ -9,6 +9,10 @@
 
 #include <app_preference.h>
 
+#include <dlog.h>
+
+#define LOG_TAG "Color Lines"
+
 TPreferenceInt::TPreferenceInt(const char *key): myKey(key), value(0) {
 	// TODO Auto-generated constructor stub
 	bool existing;
@@ -23,10 +27,12 @@ TPreferenceInt::TPreferenceInt(const char *key): myKey(key), value(0) {
 		ret = preference_get_int(key, &i_value);
 		if (ret == PREFERENCE_ERROR_NONE) {
 			value = i_value;
+			dlog_print(DLOG_DEBUG, LOG_TAG, "get key %s value = %d.", myKey, value);
 		}
 	}
 	else {
 		preference_set_int(key, value);
+		dlog_print(DLOG_DEBUG, LOG_TAG, "set key %s value = %d.", myKey, value);
 	}
 
 }
@@ -43,4 +49,5 @@ TPreferenceInt& operator+=(TPreferenceInt& left, const int& right) {
 
 void TPreferenceInt::update(){
 	preference_set_int(myKey, value);
+	dlog_print(DLOG_DEBUG, LOG_TAG, "update key %s value = %d.", myKey, value);
 }

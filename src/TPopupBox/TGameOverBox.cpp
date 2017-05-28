cc62f4950ec6503b4bda3a5b1678e1614019e42e
Alex <temper8@gmail.com>
2017-05-29 02:35:21 +0300

    add class TGameOverBox

---
+++ src/TPopupBox/TGameOverBox.cpp
@@ -0,0 +1,18 @@
+/*
+ * TGameOverBox.cpp
+ *
+ *  Created on: May 29, 2017
+ *      Author: Alex
+ */
+
+#include "TGameOverBox.h"
+
+TGameOverBox::TGameOverBox() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TGameOverBox::~TGameOverBox() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/TPopupBox/TGameOverBox.h
@@ -0,0 +1,19 @@
+/*
+ * TGameOverBox.h
+ *
+ *  Created on: May 29, 2017
+ *      Author: Alex
+ */
+
+#ifndef TPOPUPBOX_TGAMEOVERBOX_H_
+#define TPOPUPBOX_TGAMEOVERBOX_H_
+
+#include "TPopupBox.h"
+
+class TGameOverBox: public TPopupBox {
+public:
+	TGameOverBox();
+	virtual ~TGameOverBox();
+};
+
+#endif /* TPOPUPBOX_TGAMEOVERBOX_H_ */

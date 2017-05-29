1f5210ba3007444877743d456349399dac61c0e8
Alex <temper8@gmail.com>
2017-05-30 02:20:21 +0300

    удаляю TInfoBox по кнопке Back

--- src/TPopupBox/TInfoBox.cpp
+++ src/TPopupBox/TInfoBox.cpp
@@ -15,8 +15,6 @@ void popup_btn_clicked_cb(void *data, Evas_Object *obj, void *event_info)
 	pp->sendResult(1);
 }
 
-void popup_block_clicked_cb(void *data, Evas_Object *obj, void *event_info);
-
 
 TInfoBox::TInfoBox(TView *view, const char* text):TPopupBox(view) {
 	// TODO Auto-generated constructor stub
@@ -24,11 +22,11 @@ TInfoBox::TInfoBox(TView *view, const char* text):TPopupBox(view) {
 	/* popup */
 	myPopup = elm_popup_add(myView->win);
 	elm_popup_align_set(myPopup, 0.5, 0.5);
-//	eext_object_event_callback_add(popup, EEXT_CALLBACK_BACK, eext_popup_back_cb, NULL);
+	eext_object_event_callback_add(myPopup, EEXT_CALLBACK_BACK, popup_btn_clicked_cb, this);
 	evas_object_size_hint_weight_set(myPopup, EVAS_HINT_EXPAND, 0.5);
 	elm_object_text_set(myPopup, text);
 
-	evas_object_smart_callback_add(myPopup, "block,clicked", popup_block_clicked_cb, this);
+	evas_object_smart_callback_add(myPopup, "block,clicked", popup_btn_clicked_cb, this);
 
 	/* ok button */
 	btn = elm_button_add(myPopup);

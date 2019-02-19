1d50efb28992b3fa2d3ff523e8d31fb4db8b4fb3
Alex <temper8@gmail.com>
2019-02-19 15:37:43 +0300

    add TPopupBoxFactory

---
+++ src/TPopupBox/TPopupBoxFactory.cpp
@@ -0,0 +1,18 @@
+/*
+ * TPopupBoxFactory.cpp
+ *
+ *  Created on: Feb 19, 2019
+ *      Author: Alex
+ */
+
+#include "TPopupBoxFactory.h"
+
+TPopupBoxFactory::TPopupBoxFactory() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TPopupBoxFactory::~TPopupBoxFactory() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/TPopupBox/TPopupBoxFactory.h
@@ -0,0 +1,17 @@
+/*
+ * TPopupBoxFactory.h
+ *
+ *  Created on: Feb 19, 2019
+ *      Author: Alex
+ */
+
+#ifndef TPOPUPBOX_TPOPUPBOXFACTORY_H_
+#define TPOPUPBOX_TPOPUPBOXFACTORY_H_
+
+class TPopupBoxFactory {
+public:
+	TPopupBoxFactory();
+	virtual ~TPopupBoxFactory();
+};
+
+#endif /* TPOPUPBOX_TPOPUPBOXFACTORY_H_ */

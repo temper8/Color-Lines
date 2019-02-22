33adf88cd182909b470a047a1c07ced74d7f1ada
Alex <temper8@gmail.com>
2019-02-22 16:54:54 +0300

    добавил иконки

--- src/TBoardView.cpp
+++ src/TBoardView.cpp
@@ -25,17 +25,36 @@
 static void create_menu(Evas_Object *parent){
 	Evas_Object *img = NULL;
 
+	char buf[1024];
+
 	/* Create the new item */
 	Eext_Object_Item *item  = eext_more_option_item_append(parent);
 	eext_more_option_item_part_text_set(item, "selector,main_text", "test1");
 	eext_more_option_item_part_text_set(item, "selector,sub_text", "test1");
+	img = elm_image_add(parent);
+	/* Set the content in item content part */
+	eext_more_option_item_part_content_set(item, "item,icon", img);
+	snprintf(buf, sizeof(buf), "%s/music_controller_btn_play.png", app_get_resource_path());
+	elm_image_file_set(img, buf, NULL);
 
 	item  = eext_more_option_item_append(parent);
 	eext_more_option_item_part_text_set(item, "selector,main_text", "test2");
 	eext_more_option_item_part_text_set(item, "selector,sub_text", "sub_2");
+	img = elm_image_add(parent);
+	/* Set the content in item content part */
+	eext_more_option_item_part_content_set(item, "item,icon", img);
+	snprintf(buf, sizeof(buf), "%s/music_controller_btn_shuffle_on.png", app_get_resource_path());
+	elm_image_file_set(img, buf, NULL);
+
 
 	item  = eext_more_option_item_append(parent);
 	eext_more_option_item_part_text_set(item, "selector,main_text", "exit");
+	img = elm_image_add(parent);
+	/* Set the content in item content part */
+	eext_more_option_item_part_content_set(item, "item,icon", img);
+	snprintf(buf, sizeof(buf), "%s/music_controller_btn_repeat_all.png", app_get_resource_path());
+	elm_image_file_set(img, buf, NULL);
+
 
 }
 

3aafc2c6ad5e3cc9e52ef2cf0d5f239bbe8b121b
Alex <temper8@gmail.com>
2019-02-24 03:47:16 +0300

    add addItem(..)
    Переношу код для меню

--- src/TMoreOption.cpp
+++ src/TMoreOption.cpp
@@ -43,13 +43,30 @@ static void create_menu(Evas_Object *parent){
 
 
 }
+void TMoreOption::addItem(const char *mainText, const char *subText, const char *iconPath){
+	char buf[1024];
+
+	/* Create the new item */
+	Eext_Object_Item *item  = eext_more_option_item_append(more_option);
+	eext_more_option_item_part_text_set(item, "selector,main_text", mainText);
+	eext_more_option_item_part_text_set(item, "selector,sub_text", subText);
+	Evas_Object *img  = elm_image_add(more_option);
+	/* Set the content in item content part */
+	eext_more_option_item_part_content_set(item, "item,icon", img);
+	snprintf(buf, sizeof(buf), "%s/%s", app_get_resource_path(), iconPath);
+	elm_image_file_set(img, buf, NULL);   
+}
 
 TMoreOption::TMoreOption(Evas_Object *parent, const char *part){
 	more_option = eext_more_option_add(parent);
 	evas_object_show(more_option);
 	elm_object_part_content_set(parent, part, more_option);
 
-	create_menu(more_option);
+	//create_menu(more_option);
+    addItem("Option1","op1",music_controller_btn_play.png);
+    addItem("Option2","op2",music_controller_btn_shuffle_on.png);
+    addItem("Option3","op3",music_controller_btn_repeat_all.png);
+    addItem("Option4","op4",music_controller_btn_play.png);
 }
 
 TMoreOption::~TMoreOption(){

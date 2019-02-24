7e85b775c71fd1c38b0f7d4de084025b6a63fd73
Alex <temper8@gmail.com>
2019-02-25 00:20:17 +0300

    добавил кнопку OK

--- src/TPopupBox/TCircleInfoBox.cpp
+++ src/TPopupBox/TCircleInfoBox.cpp
@@ -24,6 +24,13 @@ _popup_hide_finished_cb(void *data, Evas_Object *obj, void *event_info)
 	evas_object_del(obj);
 }
 
+static void _response_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	if(!data) return;
+	elm_popup_dismiss((Evas_Object *)data);
+}
+
+
 TCircleInfoBox::TCircleInfoBox(const char* text) {
 	// TODO Auto-generated constructor stub
 	Evas_Object *layout;
@@ -34,11 +41,24 @@ TCircleInfoBox::TCircleInfoBox(const char* text) {
 	eext_object_event_callback_add(myPopup, EEXT_CALLBACK_BACK, _popup_hide_cb, NULL);
 	evas_object_smart_callback_add(myPopup, "dismissed", _popup_hide_finished_cb, NULL);
 
+
 	layout = elm_layout_add(myPopup);
 	elm_layout_theme_set(layout, "layout", "popup", "content/circle");
 
 	elm_object_part_text_set(layout, "elm.text", text);
+	evas_object_smart_callback_add(layout, "clicked", _response_cb, myPopup);
 	elm_object_content_set(myPopup, layout);
+
+
+	Evas_Object * btn = elm_button_add(myPopup);
+	elm_object_style_set(btn, "bottom");
+	elm_object_text_set(btn, "OK");
+	evas_object_size_hint_weight_set(btn, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	elm_object_part_content_set(myPopup, "button1", btn);
+	evas_object_smart_callback_add(btn, "clicked", _response_cb, myPopup);
+
+
+
 
 	evas_object_show(myPopup);
 }
